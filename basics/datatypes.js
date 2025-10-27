8025723bcf4bbf23fb5552584c6c6414ff2c5235
Akshat Gujrati <142306620+Ak-31@users.noreply.github.com>
2025-10-27 12:56:51 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,21 @@
+let var1 = "This is his name "
+let var2 = 456
+let var3 ;
+let var5 = false
+let var4 = null
+
+console.table([var1, var2, var3, var4, var5])
+
+console.table([typeof var1, typeof var2, typeof var3, typeof var4, typeof var5])
+
+
+/* Data Types
+number 
+bigint
+string 
+boolean
+*** null : null is an object but undefined is a type ***
+undefined
+symbol : Uniqueness
+object 
+*/
