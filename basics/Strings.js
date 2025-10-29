708ecbb6748ca40c05c18aa4427f11ad2ff1f2aa
Akshat Gujrati <142306620+Ak-31@users.noreply.github.com>
2025-10-29 13:20:35 +0000

    Strings in JS

---
+++ basics/Strings.js
@@ -0,0 +1,22 @@
+// *****************Strings*************************
+
+/* Strings could be concatenated: 
+let var1 = "Here"
+console.log(var1 + " its me")
+*/
+
+let var1 = new String("Akshat")
+
+console.log(typeof var1) // Returns object
+
+console.log(var1[1])
+
+console.log(var1.__proto__) // Returns empty object
+
+console.log(var1.slice(0,3)) // Do not consider the last space  // we could incorporarte negative number and according to this
+// we would consider the reverse //
+
+console.log(var1.length)
+
+console.log(var1.split())
+
